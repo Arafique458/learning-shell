2e7820d6153732a8b13f01f472c5ed3dd8fe789b
darkroom <arafique458@gmail.com>
2023-04-22 16:12:09 -0400

    Added echo statement to help recognize what is going on with the code as it runs
    echo "\e[36m>>>>>>>>>> Downloading App content <<<<<<<<<<\e[0m"

---
+++ 02-echo.sh
@@ -0,0 +1,9 @@
+echo Hello World
+
+#enable Color
+#syntac - echo -e "\e[COLmMESSAGE\e[0m"
+# -e -enable \e (enable color in echo command)
+# All the input should be there in quotes (Double quotes preferred)
+# \e - Enables a color
+# COLm - Color Code - Red()31m, Green(32m), yellow(33m), Blue(34m), Magenta(35m), Cyan(36m)
+# \e[0m to disable the color or the regular text will be printed as color.
