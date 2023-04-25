388ea4c43e5c367246fd9b27a996a0b8e61e42e0
darkroom <arafique458@gmail.com>
2023-04-24 21:16:54 -0400

    Declaring fucntion in common.sh
    added print_head for the echo statement

--- 04-function.sh
+++ 04-function.sh
@@ -8,3 +8,13 @@ function_name() {
 
 #call function
 function_name
+
+
+# we can send input to the fucntion and we can access them using special variables $1-$n, $*, $#
+
+function_name() {
+    echo First Argument = $1
+    echo Second Argument = $2
+    echo All Argument = $*
+    echo No of Argument = $#
+}
