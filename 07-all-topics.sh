30cce3ca3937b6e7bfa26c716552256976e9b68e
darkroom <arafique458@gmail.com>
2023-04-28 21:35:00 -0400

    Learning shell

---
+++ 07-all-topics.sh
@@ -0,0 +1,16 @@
+#commnet - any line starting with the # is a comment
+
+# How to write shell script file
+
+# file ending with the .sh is what we prefer to make a shell script
+
+# To run the shell script = bash script.sh
+
+# Printing
+
+# echo world
+# world will be printed on screen by ech command
+# echo we can print colors
+
+## 02.echo.sh already have those.
+
